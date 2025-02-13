b8bb3a624caac85b8a70722eec534d56f6dda6ac
congb19 <347006087@qq.com>
2025-02-13 14:36:51 +0800

    upd: retry deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  ssr: false,
+  // ssr: false,
   modules: ['@vueuse/nuxt', '@nuxt/ui'],
   colorMode: {
     preference: 'system'
