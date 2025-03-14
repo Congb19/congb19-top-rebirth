f43ac09114a7909a4ed2861a04648685ad6d5bdf
congb19 <347006087@qq.com>
2025-03-14 15:48:11 +0800

    upd: for interview test

---
+++ components/cbDialog.vue
@@ -0,0 +1,48 @@
+<script setup>
+const showDialog = ref(false);
+const emit = defineEmits(['close']);
+
+const show = () => (showDialog.value = true);
+const close = () => {
+  showDialog.value = false;
+  emit('close');
+};
+
+defineExpose({
+  show,
+  close
+});
+</script>
+<template>
+  <div class="cb-dialog-wrapper">
+    <div v-if="showDialog" class="cb-dialog">
+      <div class="cb-dialog-close-btn">
+        <UIcon name="i-heroicons-light-bulb" class="w-5 h-5" @click="close" />
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.cb-dialog-wrapper {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  pointer-events: none;
+}
+.cb-dialog {
+  width: 800px;
+  height: 600px;
+  margin: auto 0;
+  background: white;
+  pointer-events: auto;
+
+  .cb-dialog-close-btn {
+    cursor: pointer;
+    float: right;
+  }
+}
+</style>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,8 +1,115 @@
 <script setup>
+const appStore = useAppStore();
+await callOnce(appStore.fetch);
 
-const appStore = useAppStore()
-await callOnce(appStore.fetch)
+const cbDialog = ref();
+const showDialog = () => {
+  cbDialog.value?.show();
+};
 
+onMounted(() => {
+  const func1 = function (...args) {
+    console.log(this.a, ...args);
+  };
+
+  const obj1 = { a: 1 };
+
+  func1.call(obj1, [2]);
+
+  Function.prototype.myCall = function (context, ...args) {
+    // this => 函数本身
+    // context => 要成为的this
+    context = context === null ? window : Object(context);
+
+    console.log(context);
+    context['myFunc'] = this;
+
+    const res = context['myFunc'](...args);
+
+    delete context.myFunc;
+
+    return res;
+  };
+  Function.prototype.myBind = function (context, ...args) {
+    // this => fn 本身
+    // context => 目标this
+    context = context === null ? window : Object(context);
+    context['myFunc'] = this;
+    return function () {
+      return context['myFunc'](...args);
+    }
+  }
+  func1.myCall(obj1, 2, 3);
+  const func2 = func1.bind(obj1, 4);
+  func2()
+
+  Promise.myAll = function (promises) {
+    const allRes = promises.map(() => null);
+    return new Promise((resolve, reject) => {
+      promises.forEach((promise, index) => {
+        console.log(promise);
+        promise.then((res) => {
+          console.log('promise', index, res);
+          allRes[index] = res;
+          if (allRes.filter((val) => !!val).length === promises.length)
+            resolve(allRes);
+        });
+      });
+    });
+  };
+  // 实现一个自己的Promise.race
+  Promise.myRace = function (promises) {
+    return new Promise((resolve, reject) => {
+      promises.forEach((promise) => {
+        promise.then((res) => {
+          resolve(res);
+        });
+      });
+    });
+  };
+
+  const promise1 = Promise.resolve(111);
+  const promise2 = new Promise((resolve, reject) =>
+    setTimeout(() => resolve(222), 1000)
+  );
+  Promise.myAll([promise2, promise1]).then((reses) => console.log(reses));
+
+  Promise.race([promise2, promise1]).then((reses) =>
+    console.log('race', reses)
+  );
+  Promise.myRace([promise2, promise1]).then((reses) =>
+    console.log('race', reses)
+  );
+
+  const obj2 = markRaw({ aaa: 1 });
+  // vue2劫持
+  Object.defineProperty(obj2, 'aaa', {
+    get() {
+      return this.value;
+    },
+    set(val) {
+      console.log('trigger effect');
+      this.value = val;
+    },
+    enumerable: true,
+    configurable: true
+  });
+  obj2.aaa = 2;
+  console.log(obj2);
+  // vue3劫持
+  const reactiveObj2 = new Proxy(obj2, {
+    get(target, key) {
+      return target[key];
+    },
+    set(target, key, value) {
+      console.log('trigger effect');
+      target[key] = value;
+      return true;
+    }
+  });
+  reactiveObj2.aaa = 3;
+  console.log(reactiveObj2);
+});
 </script>
 
 <template>
@@ -22,6 +129,8 @@ await callOnce(appStore.fetch)
         :avatar="{ src: '/imgs/dog.jpg' }"
         title="这是林小包！"
       />
+      <CbDialog ref="cbDialog"></CbDialog>
+      <button @click="showDialog">test</button>
     </div>
   </div>
 </template>
